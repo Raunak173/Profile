b83328d8161767e2149d06490ee2fa4a9cb1efd6
Raunak173 <raunakagarwal173@gmail.com>
2022-05-30 10:05:58 +0530

    changed project section

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -33,6 +33,27 @@ export default function Projects() {
         </div>
         <div
           className="flex flex-col items-center w-64 h-72 p-10 m-6 overflow-auto shadow-2xl rounded-xl sm:w-72"
+          style={{ backgroundColor: "#ffb3c1" }}
+        >
+          <p className="mt-6 text-xl font-semibold text-center">
+            Crypto Hunter
+          </p>
+          <p className="text-base text-left md:text-center text-gray-500 leading-relaxed mt-4 justify-center">
+            This is a Crypto currency info app. I created this web app using
+            React. I used Material UI 4 for styling and Coin Gecko API for
+            information. Used Chart js 2 for Chart projections and Alice
+            Carousel for Carousel. Also deployed on Netlify. Used VS Code as
+            text editor.
+          </p>
+          <a
+            href="https://raunak-cryptozo.netlify.app/"
+            className="text-center mt-4 text-blue-600"
+          >
+            Live Link
+          </a>
+        </div>
+        <div
+          className="flex flex-col items-center w-64 h-72 p-10 m-6 overflow-auto shadow-2xl rounded-xl sm:w-72"
           style={{ backgroundColor: "#90e0ef" }}
         >
           <p className="mt-6 text-xl font-semibold text-center">I-notez</p>
@@ -66,45 +87,6 @@ export default function Projects() {
           </p>
           <a
             href="https://react-dictionary-raunak.netlify.app/"
-            className="text-center mt-4 text-blue-600"
-          >
-            Live Link
-          </a>
-        </div>
-        <div
-          className="flex flex-col items-center w-64 h-72 p-10 m-6 overflow-auto shadow-2xl rounded-xl sm:w-72"
-          style={{ backgroundColor: "#ffb3c1" }}
-        >
-          <p className="mt-6 text-xl font-semibold text-center">
-            Crypto Hunter
-          </p>
-          <p className="text-base text-left md:text-center text-gray-500 leading-relaxed mt-4 justify-center">
-            This is a Crypto currency info app. I created this web app using
-            React. I used Material UI 4 for styling and Coin Gecko API for
-            information. Used Chart js 2 for Chart projections and Alice
-            Carousel for Carousel. Also deployed on Netlify. Used VS Code as
-            text editor.
-          </p>
-          <a
-            href="https://raunak-cryptozo.netlify.app/"
-            className="text-center mt-4 text-blue-600"
-          >
-            Live Link
-          </a>
-        </div>
-        <div
-          className="flex flex-col items-center w-64 h-72 p-10 m-6 overflow-auto shadow-2xl rounded-xl sm:w-72"
-          style={{ backgroundColor: "#e0aaff" }}
-        >
-          <p className="mt-6 text-xl font-semibold text-center">Chat App</p>
-          <p className="text-base text-left md:text-center text-gray-500 leading-relaxed mt-4 justify-center">
-            A basic chat application made using React, Express, Node JS and
-            Socket.io. IDE used is VS Code. Project is also live on Netlify and
-            backend is deployed on Heroku. While working on this, I learned how
-            to work with Socket connections.
-          </p>
-          <a
-            href="https://chat-app-raunak.netlify.app"
             className="text-center mt-4 text-blue-600"
           >
             Live Link
